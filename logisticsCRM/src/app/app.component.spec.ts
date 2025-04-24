9a7d60cb4c0945e4e784b879095bd7e307fceb4d
Umar Kasan <38881409+UmarKasan@users.noreply.github.com>
2025-04-24 17:27:21 +0800

    v0.2

--- logisticsCRM/src/app/app.component.spec.ts
+++ logisticsCRM/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'logistics' title`, () => {
+  it(`should have the 'logisticsCRM' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('logistics');
+    expect(app.title).toEqual('logisticsCRM');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, logistics');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, logisticsCRM');
   });
 });
